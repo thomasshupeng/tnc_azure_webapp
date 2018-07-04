82230f43c0ea4a224a0f93e3b97bb64923365741
Shu Peng <shpeng@microsoft.com>
2018-07-03 22:06:29 -0700

    Adding port number.

--- run_waitress_server.py
+++ run_waitress_server.py
@@ -2,5 +2,4 @@ import os
 from waitress import serve
 from hello import app
 
-# serve(app, host="0.0.0.0", port=os.environ["SERVER_PORT"])
-serve(app)
+serve(app, host="0.0.0.0", port=os.environ["SERVER_PORT"])
